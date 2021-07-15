8e8eae487cd116306d5af43db51ea761c0b8c43f
szatolokin <staszatolokin28@gmail.com>
2021-07-16 00:26:22 +0300

    Add router react-router-dom Switch, Route to Main

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,13 +1,19 @@
-import React from 'react';
+import React from 'react'
 
 import './Main.css'
 
-import Welcome from './Welcome';
+import Welcome from './Welcome.js'
+import NotFound from './NotFound.js'
+
+import { Switch, Route } from 'react-router-dom'
 
 export default function Main() {
   return (
     <main className="main">
-      <Welcome />
+      <Switch>
+        <Route exact path="/" component={Welcome} />
+        <Route component={NotFound} />
+      </Switch>
     </main>
   )
 }
