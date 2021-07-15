19e9a79cebc8b4eec8b510a838e1d57a4dfc50bb
szatolokin <staszatolokin28@gmail.com>
2021-07-16 00:40:14 +0300

    Update Welcome, add SignInl, SignUp links

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -5,6 +5,8 @@ import './Welcome.css'
 import Title from './Title.js'
 import Button from './Button.js'
 
+import { Link } from 'react-router-dom'
+
 export default function Welcome() {
   return (
     <div className="welcome">
@@ -14,14 +16,18 @@ export default function Welcome() {
       <section className="welcome__section">
         <div className="welcome__row">
           <div className="welcome__col">
-            <Button>
-              Войти
-            </Button>
+            <Link to="/sign-in">
+              <Button>
+                Войти
+              </Button>
+            </Link>
           </div>
           <div className="welcome__col">
-            <Button>
-              Зарегистрироваться
-            </Button>
+            <Link to="/sign-up">
+              <Button>
+                Зарегистрироваться
+              </Button>
+            </Link>
           </div>
         </div>
       </section>
