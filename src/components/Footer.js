b1705b68300109bb31ffe2be9eab08f52371cfe5
szatolokin <staszatolokin28@gmail.com>
2021-07-15 22:56:33 +0300

    Update Footer

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './Footer.css'
+
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <a href="https://github.com/szatolokin" className="footer__link">
+        @szatolokin
+      </a>
+    </footer>
+  )
+}
