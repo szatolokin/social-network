f4acfa6cc4c4fd40bad650d0eddae586c0d60f70
szatolokin <staszatolokin28@gmail.com>
2021-07-16 00:17:34 +0300

    Add react-router-dom Link to Header, logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,13 +2,15 @@ import React from 'react';
 
 import './Header.css'
 
+import { Link } from 'react-router-dom'
+
 export default function Header() {
   return (
     <header className="header">
       <div className="header__container">
-        <a href="/" className="header__logo">
+        <Link to="/" className="header__logo">
           SN
-        </a>
+        </Link>
       </div>
     </header>
   )
