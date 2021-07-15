dce5366ffd568330a5f8290da6a9660b929f4e2d
szatolokin <staszatolokin28@gmail.com>
2021-07-15 22:56:10 +0300

    Update Header

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './Header.css'
+
+export default function Header() {
+  return (
+    <header className="header">
+      <div className="header__container">
+        <a href="/" className="header__logo">
+          SN
+        </a>
+      </div>
+    </header>
+  )
+}
